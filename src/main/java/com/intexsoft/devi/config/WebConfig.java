2ac51801ba3a787e3f2236870aa68c278a39ba60
DEVIAPHAN <broly397154>
2019-01-21 21:37:31 +0300

    Added fix for CORS (Delete)n

--- src/main/java/com/intexsoft/devi/config/WebConfig.java
+++ src/main/java/com/intexsoft/devi/config/WebConfig.java
@@ -46,7 +46,7 @@ public class WebConfig implements WebMvcConfigurer {
      */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 
     /**
